196b3430202164d768a38a9743dd8625ef52057a
wlazlok <kwlazo9@gmail.com>
2021-10-26 18:24:42 +0200

    dependency version, logs

--- ds-admin/src/main/java/karol/wlazlo/ds/admin/controllers/ProductController.java
+++ ds-admin/src/main/java/karol/wlazlo/ds/admin/controllers/ProductController.java
@@ -6,10 +6,12 @@ import karol.wlazlo.model.Admin.Product.ProductsListResponse;
 import karol.wlazlo.model.ProductItem.DeleteProductItemResponse;
 import karol.wlazlo.model.ProductItem.ProductItem;
 import karol.wlazlo.model.ProductItem.ProductItemResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/admin/products")
 public class ProductController {
@@ -29,16 +31,19 @@ public class ProductController {
 
     @GetMapping("/{productId}")
     public ResponseEntity<ProductItemResponse> fetchProductById(@PathVariable Long productId) {
+        log.info("admin.ProductController get product by ID: {}", productId);
         return dsReadClient.fetchProductDetails(productId);
     }
 
     @PostMapping("/update")
     public ResponseEntity<ProductItemResponse> updateProduct(@RequestBody ProductItem productItem) {
+        log.info("admin.ProductController update/add product with ID: {}", productItem.getId() != null ? productItem.getId() : "new");
         return dsUpdateClient.updateProduct(productItem);
     }
 
     @DeleteMapping("/delete/{productId}")
     public ResponseEntity<DeleteProductItemResponse> deleteProduct(@PathVariable("productId") Long productId) {
+        log.info("admin.ProductController delete product by ID: {}", productId);
         return dsUpdateClient.deleteProductById(productId);
     }
 }
